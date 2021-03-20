9028f3d230fde8eb32d02edf992cfb173fcfb943
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-03-20 08:34:45 +0100

    REMOVING REDUNTANT CODE
    - moving reduntant code to separate file

--- tower.py
+++ tower.py
@@ -17,58 +17,28 @@ def to_battle():
     time.sleep(3)
     to_battle_sign = pyautogui.locateOnScreen('/home/bart/Pictures/hero_wars/to_battle.png', confidence=0.8)
     if to_battle_sign:
-        middle_X = to_battle_sign.left + 15
-        middle_Y = to_battle_sign.top + 15
-        try:
-            pyautogui.click(x=middle_X, y=middle_Y)
-        except AttributeError:
-            pass
+        locate_button_coords(to_battle_sign)
     else:
         to_battle_sign = pyautogui.locateOnScreen('/home/bart/Pictures/hero_wars/to_battle.png', confidence=0.8)
         if to_battle_sign:
-            middle_X = to_battle_sign.left + 15
-            middle_Y = to_battle_sign.top + 15
-            try:
-                pyautogui.click(x=middle_X, y=middle_Y)
-            except AttributeError:
-                pass            
+            locate_button_coords(to_battle_sign)
 
 
 def skip_battle():
     time.sleep(3)
     skip_battle = pyautogui.locateOnScreen('/home/bart/Pictures/hero_wars/skip.png', confidence=0.8)
     if skip_battle:
-        middle_X = skip_battle.left + 15
-        middle_Y = skip_battle.top + 15
-        try:
-            pyautogui.click(x=middle_X, y=middle_Y)
-        except AttributeError:
-            pass
+        locate_button_coords(skip_battle)
     else:
         skip_battle = pyautogui.locateOnScreen('/home/bart/Pictures/hero_wars/to_battle.png', confidence=0.8)
         if skip_battle:
-            middle_X = skip_battle.left + 15
-            middle_Y = skip_battle.top + 15
-            try:
-                pyautogui.click(x=middle_X, y=middle_Y)
-            except AttributeError:
-                pass 
+            locate_button_coords(skip_battle)
 
 def click_on_buff():
     buff_tower = pyautogui.locateOnScreen('/home/bart/Pictures/hero_wars/buffer_tower.png', confidence=0.8)
     if buff_tower:
-        middle_X = buff_tower.left + 15
-        middle_Y = buff_tower.top +15
-        try:
-            pyautogui.click(x=middle_X, y=middle_Y)
-        except AttributeError:
-            pass
+        locate_button_coords(buff_tower)
     else:
         buff_tower = pyautogui.locateOnScreen('/home/bart/Pictures/hero_wars/buffer_tower.png', confidence=0.8)
         if buff_tower:
-            middle_X = buff_tower.left + 15
-            middle_Y = buff_tower.top +15
-            try:
-                pyautogui.click(x=middle_X, y=middle_Y)
-            except AttributeError:
-                pass
+            locate_button_coords(buff_tower)
